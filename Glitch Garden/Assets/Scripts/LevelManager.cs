3d53a0caf9ffaf17641a017a83a488d5010f3f81
Dimitar <dimdimvas@yahoo.com>
2017-09-05 18:30:24 -0600

    Glitch Garden splash screen created. Start and Options Menu control flow added. Music included in splash, start, and level 1 screen.

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public float autoLoadNextLevelAfter;
+	
+	void Start(){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+	
+	public void LoadLevel(string name){
+
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+	
+	public void BackToStart(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void LoadNextLevel(){
+
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	
+}
---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+		audioSource = GetComponent<AudioSource>();
+		Debug.Log ("Don't destroy on load: " + gameObject);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnLevelWasLoaded(int level){
+		Debug.Log ("Playing clip: " + levelMusicChangeArray[level]);
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		audioSource.clip = thisLevelMusic;
+		audioSource.loop = true;
+		audioSource.Play();
+	}
+}
