9f6e5b260dd529fc3d3376e466e7d5d26057073b
Dimitar <dimdimvas@yahoo.com>
2017-10-01 15:03:30 -0700

    BowlMaster game started - Added floor to scene - Added 10 pins to the end of the floor according to GDD (Game Design Document) - Added ball on the other end of floor 	+ Ball initialized with Ball.cs script 	+ Moves from one end of board to the other

---
+++ BowlMaster/Assets/START Pack/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+		
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
