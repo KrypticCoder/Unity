f46a275e5762ff4892d1b4acdef6b4c010d108e4
Dimitar <dimdimvas@yahoo.com>
2017-10-19 14:34:58 -0700

    - Swiper added to lane to knock down pins - Number of pins left standing kept track of in PinSetter

--- BowlMaster/Assets/Scripts/Ball.cs
+++ BowlMaster/Assets/Scripts/Ball.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
 	public Vector3 launchVelocity;
+    public bool inPlay = false;
+
 
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
-
+    
 	// Use this for initialization
 	void Start () {
-
 		audioSource = GetComponent<AudioSource>();
-		audioSource.Play();
-		
+
 		rigidBody = GetComponent<Rigidbody>();
-		rigidBody.velocity = launchVelocity;
+		rigidBody.useGravity = false;
+		
+	}
+
+	public void Launch(Vector3 velocity){
+
+        inPlay = true;
+
+		audioSource.Play();
+
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
 	}
 	
 	// Update is called once per frame
---
+++ BowlMaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+
+    public float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public bool IsStanding(){
+    	
+       	Vector3 rotationInEuler = transform.rotation.eulerAngles;
+       	float tiltInX = rotationInEuler.x;
+       	float tiltInZ = rotationInEuler.z;
+
+       	if(tiltInX < standingThreshold && tiltInZ < standingThreshold)
+       		return true;
+       	else
+       		return false;
+
+    }
+}
