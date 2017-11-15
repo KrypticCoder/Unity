3de7160c29578617b82b8753c4927e360f3c4b5f
Dimitar <dimdimvas@yahoo.com>
2017-11-14 23:49:46 -0800

    ScoreFrames() calcalates the score in each frame given a list of rolls. If not a strike or spare, each frame is the result of two rolls. If strike, frame score is 10 + score for next two rolls. If spare, frame score is 10 + score for next roll. ScoreCumulative() takes the list of frame scores and calculates a running total for each frame.

--- BowlMaster/Assets/Scripts/ScoreMaster.cs
+++ BowlMaster/Assets/Scripts/ScoreMaster.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class ScoreMaster {
 
 	// Returns a list of cumulative scores like a normal score card
-	public static List<int> ScoreCumulative(List<int> roles){
+	public static List<int> ScoreCumulative(List<int> rolls){
 		List<int> cumulativeScores = new List<int>();
 
-		int runningTotal;
+		int runningTotal = 0;
 
-		foreach(int frameScore in ScoreFrames(roles)){
+		foreach(int frameScore in ScoreFrames(rolls)){
 			runningTotal += frameScore;
 			cumulativeScores.Add(runningTotal);
 		}
@@ -19,10 +19,70 @@ public class ScoreMaster {
 	}
 
 	// Returns a list of individual frame scores, NOT cumulative
-	public static List<int> ScoreFrames(List<int> roles){
-		List<int> frameList = new List<int>();
+	public static List<int> ScoreFrames(List<int> rolls){
+		List<int> frames = new List<int>();
 
-		return frameList;
+		for(int i = 1; i < rolls.Count; i+=2){
+
+			if(frames.Count == 10) { break; }
+
+			if(rolls[i-1] + rolls[i] < 10){
+				frames.Add(rolls[i - 1] + rolls[i]);
+			}
+
+			if(rolls.Count - i <= 1) { break; }
+
+			if(rolls[i-1] == 10){
+				i--;
+				frames.Add(10 + rolls[i+1] + rolls[i+2]);
+			} else if(rolls[i-1] + rolls[i] == 10){
+				frames.Add(10 + rolls[i+1]);
+			}
+		}
+
+		string s = "frames = ";
+		foreach(int frame in frames){
+			s += frame.ToString();
+		}
+		Debug.Log(s);
+
+		return frames;
 	}
+
+//	// Returns a list of individual frame scores, NOT cumulative
+//	public static List<int> ScoreFrames(List<int> rolls){
+//		List<int> frameList = new List<int>();
+//		int count = 0;
+//
+//		for(int i = 0; i < rolls.Count; i++){
+//
+//			// check if strike
+//			if(rolls[i] == 10){
+//	
+//
+//				if(i + 2 < rolls.Count && frameList.Count < 10){
+//					frameList.Add(10 + rolls[i + 1] + rolls[i + 2]);
+//				}
+//				count += 1;
+//			}
+//
+//			else if((i + 1) - count == 2 && frameList.Count < 10){
+//				int score = rolls[i] + rolls[i - 1];
+//
+//				// check if spare
+//				if(score == 10 && i + 1 < rolls.Count){
+//					score += rolls[i + 1];
+//					frameList.Add(score);
+//
+//				// add if not in last frame, otherwise spare in last frame
+//				} else if(score != 10){
+//					frameList.Add(score);
+//				}
+//				count += 2;
+//			} 
+//		}
+//	
+//		return frameList;
+//	}
 
 }
